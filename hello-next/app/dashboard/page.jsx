1a523c47a84a660311df57ae2ebe892c878a9fd1
Sunbeom <wsbs9805@naver.com>
2024-07-29 14:21:16 +0900

    경로 생략하기 및 공통 레이아웃 작성하기

---
+++ hello-next/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LineChart from "../../components/line-chart";
+
+const DashBoard = () => {
+  return (
+    <>
+      <div>DashBoard</div>
+      <LineChart />
+    </>
+  );
+};
+
+export default DashBoard;
